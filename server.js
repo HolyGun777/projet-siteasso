80f4cc25385a3beaac20ab43cfbad17e9e9ed10e
HolyGun777 <eddyleguen@outlook.fr>
2022-07-06 16:43:02 +0200

    base site assos

--- server.js
+++ server.js
@@ -23,23 +23,34 @@ app.get('/', function (req, res) {
     res.render('home')
 })
 
-app.get('/accueil', function (req, res) {
-    res.render('accueil')
-})
+
 app.get('/article', function (req, res) {
     res.render('article')
 })
 
-app.get('/ecole-du-mans', function (req, res) {
-    res.render('ecole-du-mans')
+app.get('/biographie', function (req, res) {
+    res.render('biographie')
 })
 
 app.get('/formulaire', function (req, res) {
     res.render('formulaire')
 })
 
+app.get('/inscription', function (req, res) {
+    res.render('inscription')
+})
+
 app.get('/login', function (req, res) {
     res.render('login')
 })
+
+app.get('/sitepartenaire', function (req, res) {
+    res.render('sitepartenaire')
+})
+
 // Run server
-app.listen(3000);
+// app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log(`Ecoute le port ${port}, lancé le : ${new Date().toLocaleString()}`);
+});
